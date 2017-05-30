b3892cf7940e12908f4e76164449d5b193caec4a
US\santhor <sashr17@gmail.com>
2017-05-30 20:11:55 +0530

    1. Added axios for http calls 2. added styling for home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
+import axios from 'axios';
+import './Home.css';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('https://api.myjson.com/bins/8phdd')
+            .then(res => {
+                const posts = res.data['Task-details'];
+                this.setState({
+                    posts
+                });
+            });
+    }
+
     render() {
         return (
-            <h2>Home Component</h2>
+            <div className='home-component'>
+                <h2> Basic Task fields </h2>
+                <ul>
+                {
+                    this.state.posts.map((post, index) =>
+                    <li key={index}>{post}</li>)
+                }
+                </ul>
+            </div>
         );
     }
 }
